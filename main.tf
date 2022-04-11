3958da940cc2744077fefb20506f811b4db6d6c8
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2022-04-12 02:12:16 +0530

    Add route and route table

--- main.tf
+++ main.tf
@@ -26,3 +26,17 @@ resource "aws_internet_gateway" "dev_internet_gateway" {
     name = "dev-igw"
   }
 }
+
+resource "aws_route_table" "dev_public_rt" {
+    vpc_id = aws_vpc.dev_vpc.id
+
+    tags = {
+        name = "dev-public-rt"
+    }
+}
+
+resource "aws_route" "default_route" {
+    route_table_id = aws_route_table.dev_public_rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev_internet_gateway.id
+}
