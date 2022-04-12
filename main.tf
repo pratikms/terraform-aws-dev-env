2ce06886a246772857193fbf6d4cc980a272096c
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2022-04-13 01:23:48 +0530

    Add EC2 instance and userdata

--- main.tf
+++ main.tf
@@ -70,3 +70,20 @@ resource "aws_key_pair" "dev_auth" {
   key_name = "dev"
   public_key = file("~/.ssh/dev.pub")
 }
+
+resource "aws_instance" "dev_node" {
+  instance_type = "t2.micro"
+  ami = data.aws_ami.server_ami.id
+  key_name = aws_key_pair.dev_auth.key_name
+  vpc_security_group_ids = [aws_security_group.dev_sg.id]
+  subnet_id = aws_subnet.dev_public_subnet.id
+  user_data = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    name = "dev-node"
+  }
+}
